46adeb744e844e6f1ec7304261c9f65d995430e6
唐师傅 <shengdongtang8@gmail.com>
2025-03-01 16:21:52 +0800

    feat(backend): 初始化 Emby Telegram Miniapp 项目
    - 创建 Maven 项目结构和配置文件
    - 添加 Spring Boot 启动类和测试类
    - 设置应用属性和依赖
    - 配置 Git属性和忽略文件
    - 添加 IDE 相关配置
    - 设置 Maven Wrapper 脚本和属性

---
+++ backend/src/main/java/tdck/backend/embytelegramminiapp/EmbyTelegramMiniappApplication.java
@@ -0,0 +1,13 @@
+package tdck.backend.embytelegramminiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbyTelegramMiniappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbyTelegramMiniappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tdck/backend/embytelegramminiapp/EmbyTelegramMiniappApplicationTests.java
@@ -0,0 +1,13 @@
+package tdck.backend.embytelegramminiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbyTelegramMiniappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
